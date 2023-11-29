96a0b68692d3d5b34f314193bca55ba51c61fd22
thermvs <mariagewitter@gmail.com>
2023-11-29 16:26:14 +0300

    feat: add initial configuration

---
+++ src/main/java/artgallery/hscms/HsCmsApplication.java
@@ -0,0 +1,13 @@
+package artgallery.hscms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsCmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/artgallery/hscms/HsCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package artgallery.hscms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsCmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
