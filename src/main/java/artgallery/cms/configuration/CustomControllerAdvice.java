95ae7c8fe945af4d239a299588162379e71d0333
Nikita Kulakov <nvkulakov.dev@gmail.com>
2023-12-17 12:39:08 +0300

    fix: return message instead of debug on error

--- src/main/java/artgallery/cms/configuration/CustomControllerAdvice.java
+++ src/main/java/artgallery/cms/configuration/CustomControllerAdvice.java
@@ -8,7 +8,6 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -17,13 +16,13 @@ public class CustomControllerAdvice extends ResponseEntityExceptionHandler {
   @ResponseBody
   @ExceptionHandler({  AccessDeniedException.class })
   public ResponseEntity<?> handleAuthenticationException(Exception ex) {
-    return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ApiErrorDTO(HttpStatus.FORBIDDEN, "Authentication failed", ex));
+    return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ApiErrorDTO(HttpStatus.FORBIDDEN, "Authentication failed", ex.getMessage()));
   }
 
   @ResponseBody
   @ExceptionHandler({DoesNotExistException.class })
   public ResponseEntity<?> handleDoesNotExistException(DoesNotExistException ex) {
-    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiErrorDTO(HttpStatus.NOT_FOUND, ex));
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiErrorDTO(HttpStatus.NOT_FOUND, ex.getMessage()));
   }
 
 }
